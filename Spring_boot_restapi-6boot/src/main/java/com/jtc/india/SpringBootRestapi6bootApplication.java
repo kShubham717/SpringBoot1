35da1011852a75e7cc6feb4d15d2a898d8da3912
kshubham <test.kshubham@gmail>
2024-05-15 13:39:58 +0530

    commited

---
+++ Spring_boot_restapi-6boot/src/main/java/com/jtc/india/SpringBootRestapi6bootApplication.java
@@ -0,0 +1,13 @@
+package com.jtc.india;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestapi6bootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestapi6bootApplication.class, args);
+	}
+
+}
